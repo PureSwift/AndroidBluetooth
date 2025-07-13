1e95934099f56dfe051e7efc4a19c6293b6a167d
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-13 19:16:23 -0400

    Add `AndroidCentralError`

---
+++ Sources/AndroidBluetooth/AndroidCentralError.swift
@@ -0,0 +1,19 @@
+//
+//  AndroidCentralError.swift
+//  AndroidBluetooth
+//
+//  Created by Alsey Coleman Miller on 7/13/25.
+//
+
+/// Android Central Error
+public enum AndroidCentralError: Swift.Error {
+    
+    /// Bluetooth is disabled.
+    case bluetoothDisabled
+    
+    /// Binder IPC failure.
+    case binderFailure
+    
+    /// Unexpected null value.
+    case nullValue(AnyKeyPath)
+}
