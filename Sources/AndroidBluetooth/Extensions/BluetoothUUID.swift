b0411627d8cde0c00e00ca9ec0463b60582dc6bc
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-13 19:15:14 -0400

    Add `BluetoothUUID` extensions

---
+++ Sources/AndroidBluetooth/Extensions/BluetoothUUID.swift
@@ -0,0 +1,27 @@
+//
+//  BluetoothUUID.swift
+//  AndroidBluetooth
+//
+//  Created by Alsey Coleman Miller on 7/13/25.
+//
+
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#elseif canImport(Foundation)
+import Foundation
+#endif
+import JavaUtil
+import Bluetooth
+
+internal extension BluetoothUUID {
+    
+    init(android javaUUID: JavaUtil.UUID) {
+        
+        let uuid = UUID(uuidString: javaUUID.toString())!
+        if let value = UInt16(bluetooth: uuid) {
+            self = .bit16(value)
+        } else {
+            self = .bit128(UInt128(uuid: uuid))
+        }
+    }
+}
