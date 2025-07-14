bb46879fbefe3c4cc375f94cdbb1f3fbbfd14d8a
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-13 21:08:23 -0400

    Add `BluetoothConnectionState`

---
+++ Sources/AndroidBluetooth/BluetoothConnectionState.swift
@@ -0,0 +1,51 @@
+//
+//  BluetoothConnectionState.swift
+//  AndroidBluetooth
+//
+//  Created by Alsey Coleman Miller on 7/13/25.
+//
+
+import JavaKit
+
+/// Connection State
+public enum BluetoothConnectionState: Equatable, Hashable, Sendable, CaseIterable {
+    
+    /// The profile is in connected state
+    case connected
+    
+    case connecting
+    
+    case disconnecting
+    
+    case disconnected
+}
+
+internal extension BluetoothConnectionState {
+    
+    static let javaClass = try! JavaClass<BluetoothProfile>()
+}
+
+// MARK: - RawRepresentable
+
+extension BluetoothConnectionState: RawRepresentable {
+    
+    public init?(rawValue: Int32) {
+        guard let value = Self.allCases.first(where: { $0.rawValue == rawValue }) else {
+            return nil
+        }
+        self = value
+    }
+    
+    public var rawValue: Int32 {
+        switch self {
+        case .connected:
+            Self.javaClass.STATE_CONNECTED
+        case .connecting:
+            Self.javaClass.STATE_CONNECTING
+        case .disconnecting:
+            Self.javaClass.STATE_DISCONNECTING
+        case .disconnected:
+            Self.javaClass.STATE_DISCONNECTED
+        }
+    }
+}
